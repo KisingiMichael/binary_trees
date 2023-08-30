e093499ba8c5f63c1e3712bfbbb2527177393d53
KisingiMichael <kisingimichael12@gmail.com>
2023-08-29 19:27:32 -0700

    Fix more output errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,11 +9,10 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	 binary_tree_t *new_left_node;
+
 	if (parent == NULL)
 		return (NULL);
-
-	binary_tree_t *new_left_node;
-
 	new_left_node = malloc(sizeof(binary_tree_t));
 	if (new_left_node == NULL)
 		return (NULL);
